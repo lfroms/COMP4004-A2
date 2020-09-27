085246cec5dd4693a4484440fc03393315a9b153
Lukas Romsicki <lukas@romsicki.com>
2020-09-27 14:23:47 -0400

    [CODE] Add implementation for hasWinner and getWinnerId

--- src/main/java/server/ScoreCard.java
+++ src/main/java/server/ScoreCard.java
@@ -2,6 +2,11 @@ package server;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 final class ScoreCard {
@@ -17,5 +22,32 @@ final class ScoreCard {
 
 	public List<Score> getScores(Integer playerId) {
 		return scores.stream().filter(score -> score.getPlayerId() == playerId).collect(Collectors.toList());
+	}
+
+	public Boolean hasWinner() {
+		return getWinnerId().isPresent();
+	}
+
+	public Optional<Integer> getWinnerId() {
+		Integer numberOfPlayers = scores.stream().filter(distinctByKey(p -> p.getPlayerId()))
+				.collect(Collectors.toList()).size();
+
+		for (int i = 0; i < numberOfPlayers; i++) {
+			final Integer id = i + 1;
+
+			Integer playerTotal = scores.stream().filter(s -> s.getPlayerId() == id).reduce(0,
+					(accumulator, object) -> accumulator + object.getScore(), Integer::sum);
+
+			if (playerTotal >= 6000) {
+				return Optional.of(id);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	private static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+		Map<Object, Boolean> map = new ConcurrentHashMap<>();
+		return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
 	}
 }
