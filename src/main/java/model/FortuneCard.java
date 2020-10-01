3aa4ffa226754a0989faf9bd1e05bb4ed2d1ebe1
Lukas Romsicki <lukas@romsicki.com>
2020-10-01 17:37:23 -0400

    [CODE] Add implementation for FortuneCard numerical value

--- src/main/java/model/FortuneCard.java
+++ src/main/java/model/FortuneCard.java
@@ -1,17 +1,45 @@
 package model;
 
+import java.util.Random;
+
 public final class FortuneCard {
 	private FortuneCardType type = null;
+	private Integer numericalValue = 0;
+
+	Random random = new Random();
 
 	public FortuneCard() {
 		type = FortuneCardType.random();
+		numericalValue = getRandomNumericalValue();
 	}
 
 	public FortuneCard(FortuneCardType type) {
 		this.type = type;
+		numericalValue = getRandomNumericalValue();
+	}
+
+	public FortuneCard(FortuneCardType type, Integer numericalValue) {
+		this.type = type;
+		this.numericalValue = numericalValue;
 	}
 
 	public FortuneCardType getType() {
 		return type;
+	}
+
+	public Integer getNumericalValue() {
+		return numericalValue;
+	}
+
+	private Integer getRandomNumericalValue() {
+		if (type == FortuneCardType.SKULLS) {
+			return random.nextInt() * 2 + 1;
+		}
+
+		if (type == FortuneCardType.SEA_BATTLE) {
+			return random.nextInt() * 4 + 1;
+		}
+
+		return 0;
 	}
 }
