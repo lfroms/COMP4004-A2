6424a3069d4467a5bcf12dbb286426b8521be272
Lukas Romsicki <lukas@romsicki.com>
2020-10-16 20:05:50 -0400

    Add cucumber dependencies and test class

---
+++ src/test/java/server/CucumberTest.java
@@ -0,0 +1,11 @@
+package server;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" }, monochrome = true)
+public class CucumberTest {
+}
